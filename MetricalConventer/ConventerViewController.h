1b532c41b9bea2e9217c196e3cdb7290ab106b22
Sergey Klimov <dcdarv@gmail.com>
2011-11-05 23:59:55 +0200

    added view controller

---
+++ MetricalConventer/ConventerViewController.h
@@ -0,0 +1,16 @@
+//
+//  ConventerViewController.h
+//  MetricalConventer
+//
+//  Created by Sergey Klimov on 05.11.11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "Conventer.h"
+
+@interface ConventerViewController : UIViewController
+{
+    Conventer * conventer;
+}
+@end
